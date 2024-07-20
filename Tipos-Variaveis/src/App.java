8fd12c2b0c4e9d0aec16c0a5837f507891d0fe72
Gustavo Valença Marcilio <workgustavo10@gmail.com>
2024-07-19 23:08:02 -0300

    Add files via upload

--- Tipos-Variaveis/src/App.java
+++ Tipos-Variaveis/src/App.java
@@ -1,5 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500.33;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+
+        numero = 2;
+
+        final double valor_de_Pi = 3.14;
+
+        double valor_de_Pi = 15.3;
+
+
     }
 }
