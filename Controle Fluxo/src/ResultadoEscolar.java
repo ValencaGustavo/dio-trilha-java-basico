8c69f30bd0d128241a27945098299d873010f3af
Gustavo Valença Marcilio <workgustavo10@gmail.com>
2024-08-10 19:48:26 -0300

    Add files via upload

---
+++ Controle Fluxo/src/ResultadoEscolar.java
@@ -0,0 +1,17 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+
+        int nota = 7;
+
+        if (nota >= 7) {
+            System.out.println("Aprovado");
+
+        } else if (nota >= 5 && nota < 7) {
+            System.out.println("Prova recuperação");
+
+        } else {
+            System.err.println("Reprovado");
+            
+        }
+    }
+}
